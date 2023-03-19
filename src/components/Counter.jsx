59e3d77d45b9c5d7b99e838aae5c788df4372119
denis-larin-22 <denislarin2017@gmail.com>
2023-03-19 17:30:18 +0200

    added countAction

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,20 @@
-
 import { useReducer } from 'react';
 
 const Counter = () => {
     const inittialState = {
         count: 0,
-        lastAction: null
+        lastAction: null,
+        countAction: 0
     }
 
     const reducer = (state, action) => {
         switch (action.type) {
             case 'Increment':
-                return { count: state.count + 1, lastAction: 'Increment' }
+                return { count: state.count + 1, lastAction: 'Increment', countAction: state.countAction + 1 }
             case 'Decrement':
-                return { count: state.count - 1, lastAction: 'Decrement' }
+                return { count: state.count - 1, lastAction: 'Decrement', countAction: state.countAction + 1 }
             case 'Reset':
-                return { count: 0, lastAction: 'Reset' }
+                return { count: 0, lastAction: 'Reset', countAction: state.countAction + 1 }
             default:
                 return state;
         }
@@ -38,6 +38,7 @@ const Counter = () => {
         <div>
             <h1>Counter: {state.count}</h1>
             <h3>The last action: {state.lastAction}</h3>
+            <h4>Total actions: {state.countAction}</h4>
             <button onClick={hendleCountIncrement}>Increment</button>
             <button onClick={hendleCountDecrement}>Decrement</button>
             <button onClick={hendleCountReset}>Reset</button>
